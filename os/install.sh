d2c0796a74d427b2a4e7c8219ff839edd8ef2147
luismerinou <lmerinoulizarna@gmail.com>
2022-02-09 13:28:45 +0100

    add gitignore

--- os/install.sh
+++ os/install.sh
@@ -12,7 +12,7 @@ set_macos_defaults() {
 
     # Set a blazingly fast keyboard repeat rate
     # defaults write NSGlobalDomain KeyRepeat -int 1
-    # defaults write NSGlobalDomain InitialKeyRepeat -int 10
+    defaults write NSGlobalDomain InitialKeyRepeat -int 12
 
 
     ###############################################################################
--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,9 +2,16 @@
 alias ..='cd ../'
 alias ...='cd ../../'
 alias ....='cd ../../../'
-
 alias cl='clear'
-
+alias webstorm='~/webstorm'
 alias l='ls -laGFhS'
 alias ls='ls -aGFhS'
 alias lg='l | grep --color=auto'
+#cd + ls
+cd() { builtin cd "$@"; ls; }
+# ip 
+alias ip='ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk "{print $2}"'
+alias publicIp="curl https://ipinfo.io/ip"
+# mueve a papelera sustituye a rm -rf
+trash () { command mv "$@" ~/.Trash; }
+
